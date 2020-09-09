d7a96eb20a103f4b9738ff2e0799dcf769e3f3aa
Grace Anconetani <grace.anconetani@gmail.com>
2020-09-08 21:12:32 -0500

    client

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/', (req, res) => {
-  res.send("Hello World");
+// Put all API endpoints under '/api'
+app.get('/api/signs', (req, res) => {
+  const count = 2;
+
+  // Generate some passwords
+  const signs = [
+    "TEST SIGN 1",
+    "TEST SIGN 2"
+  ]
+
+  // Return them as json
+  res.json(signs);
+
+  console.log(`Sent ${count} signs`);
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 app.listen(process.env.PORT, () =>
