61a15de5c37df27399b786f0f1cc2cef5307df6b
Grace Anconetani <grace.anconetani@gmail.com>
2020-09-09 12:11:34 -0500

    sign diff

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Put all API endpoints under '/api'
@@ -11,15 +14,38 @@ app.get('/api/signs', (req, res) => {
   const count = 2;
 
   // Generate some passwords
-  const signs = [
-    "TEST SIGN 1",
-    "TEST SIGN 2"
-  ]
+  const previous = "BEST REALTOR ONE LINER: CAN YOU SEE YOURSELF QUARANTINING HERE?";
+  const next = "I HEARD THE GOVERNMENT IS PUTTING CHIPS INSIDE PEOPLE. I HOPE I GET TORTILLA CHIPS!";
 
   // Return them as json
-  res.json(signs);
+  res.json({ previous: previous, next: next });
 
   console.log(`Sent ${count} signs`);
+});
+
+app.post('/api/diff', (req, res) => {
+  console.log(req.body.previous);
+  console.log(req.body.next);
+
+  let next = req.body.next.replace(/\s/g, "");
+  let previous = req.body.previous;
+  let remove = "";
+  let pull = "";
+
+  previous.split('').forEach((letter) => {
+    if (letter != " ") {
+      let index = next.indexOf(letter);
+      if (index > -1) {
+        next = next.replace(letter, '');
+      } else {
+        remove += letter;
+      }
+    }
+  });
+
+  console.log("Remove: " + remove);
+  console.log("Pull: " + next);
+  res.json({ pull: next, remove: remove });
 });
 
 // The "catchall" handler: for any request that doesn't
--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import logo from './ealogo.png';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  Input,
+  Label,
+  Button,
+  Container,
+  Row, Col,
+  Form,
+  FormGroup
+} from 'reactstrap';
+
+class App extends Component {
+
+  state = { previous: "", next: "", pull: "", remove: "" };
+
+  diffSigns = (e) => {
+    e.preventDefault();
+    axios.post('/api/diff', { previous: this.state.previous, next: this.state.next })
+        .then(response => this.setState({ pull: response.data.pull, remove: response.data.remove }));
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value.toUpperCase()
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <Container className="m-5">
+            <Row className="justify-content-center">
+              <Col md="6">
+                <Form onSubmit={this.diffSigns}>
+                  <FormGroup>
+                    <Label>Previous</Label>
+                    <Input name="previous" placeholder="Previous Sign" onChange={this.handleChange} required/>
+                  </FormGroup>
+                  <FormGroup>
+                    <Label>Next</Label>
+                    <Input name="next" placeholder="Next Sign" onChange={this.handleChange} required/>
+                  </FormGroup>
+                  <Button type="submit">Diff Signs</Button>
+                </Form>
+
+              </Col>
+            </Row>
+            <Row className="justify-content-center mt-3">
+              <Col md="6">
+                <p>Pull: {this.state.pull}</p>
+                <p>Remove: {this.state.remove}</p>
+              </Col>
+            </Row>
+          </Container>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
